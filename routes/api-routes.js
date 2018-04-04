d26abe774e9a3f5288beb01b5fc6b469e9f87473
Leif Swenson <leifswenson@gmail.com>
2018-04-04 21:46:45 +0000

    added cheerio

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@
 // ============================================================================
 var db = require("../models");
 var axios = require("axios");
+var cheerio = require("cheerio");
 
 module.exports = function(app) {
 
